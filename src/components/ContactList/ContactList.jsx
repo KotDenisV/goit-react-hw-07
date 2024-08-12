aabc314cbe4bcbcd5ae89dcc2d59ed5aba4fcc8b
KotDenisV <kot.denis@gmail.com>
2024-08-13 01:49:17 +0300

    Memo

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,12 +1,10 @@
 import { useSelector } from 'react-redux';
 import Contact from '../Contact/Contact';
 import s from './ContactList.module.css';
-import { selectContacts, selectFilter } from '../../redux/selectors';
+import { selectFilteredContacts } from '../../redux/selectors';
 
 function ContactList() {  
-  const contacts = useSelector(selectContacts) || [];  
-  const filter = useSelector(selectFilter) || '';
-  const filteredData = contacts.filter(item => item.name.toLowerCase().includes(filter.toLowerCase()));
+    const filteredData = useSelector(selectFilteredContacts);
   
     return (
     <ul className={s.list}>
